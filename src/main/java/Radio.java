b009348f430ef86884c39bd047863f1d1cd16c3c
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-11-01 17:49:12 +0300

    Initial coomit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,69 @@
+public class Radio {
+    private int currentRadioStationNumber;
+    private int soundVolume;
+    public int getCurrentRadioStationNumber() {
+        return currentRadioStationNumber;
+    }
+    public void CurrentRadioStationNumber(int newCurrentRadioStationNumber) {
+        if (newCurrentRadioStationNumber < 0) {
+            return;
+        }
+        if (newCurrentRadioStationNumber > 9) {
+            return;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber+1 ;
+    }
+    public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) {
+        if (newCurrentRadioStationNumber < 0) {
+            return;
+        }
+        if (newCurrentRadioStationNumber > 9) {
+            return;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber ;
+    }
+    public int nextStation(int radioStationMore9) {
+        if (radioStationMore9 >= 9) {
+            return currentRadioStationNumber = 0;
+        }
+        return currentRadioStationNumber = radioStationMore9 + 1;
+    }
+    public int prevStation(int radioStationLess0) {
+        if (radioStationLess0 <= 0) {
+            return currentRadioStationNumber = 9;
+        }
+        return currentRadioStationNumber = radioStationLess0 - 1;
+    }
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+    public void setSoundVolume(int newSoundVolume) {
+        if (newSoundVolume < 0) {
+            return;
+        }
+        if (newSoundVolume > 10) {
+            return;
+        }
+        soundVolume = newSoundVolume+1 ;
+    }
+
+    public int moreMaxVolume(int maxVolume) {
+
+        if(maxVolume>=10 ) {
+            return soundVolume=10;
+        }
+        return soundVolume=maxVolume+1;
+    }
+
+    public int lessMinVolume(int minVolume) {
+
+        if(minVolume<=0 ) {
+            return soundVolume=0;
+        }
+        return soundVolume=minVolume-1;
+    }
+}
+
+
+
+
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void shouldSetStation() {
+        Radio number = new Radio();
+        number.setCurrentRadioStationNumber(5);
+        int expected = 5;
+        int actual = number.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationMore1() {
+        Radio number = new Radio();
+        number.CurrentRadioStationNumber(5);
+        int expected = 6;
+        int actual = number.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationMore9() {
+        Radio number = new Radio();
+        number.nextStation(9);
+        int expected = 0;
+        int actual = number.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationLess0() {
+        Radio number = new Radio();
+        number.prevStation(0);
+        int expected = 9;
+        int actual = number.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolume() {
+        Radio volumeNumber = new Radio();
+        volumeNumber.setSoundVolume(5);
+        int expected = 6;
+        int actual = volumeNumber.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setVolumeMoreMax(){
+        Radio volumeNumber = new Radio();
+        volumeNumber.moreMaxVolume(10);
+        int expected=10;
+        int actual=volumeNumber.getSoundVolume();
+        Assertions.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void setVolumeLessMin(){
+        Radio volumeNumber = new Radio();
+        volumeNumber.lessMinVolume(0);
+        int expected=0;
+        int actual=volumeNumber.getSoundVolume();
+        Assertions.assertEquals(expected,actual);
+
+    }
+
+
+}
